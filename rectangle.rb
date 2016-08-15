e3772e312b0ddc06a2236aea673bdf53756bcdca
lsewilson <laurawilson@hotmail.co.uk>
2016-08-15 20:32:19 +0100

    error message added to rectangle method

--- rectangle.rb
+++ rectangle.rb
@@ -1,25 +1,23 @@
 def top(w)
-  puts  "-" * w
+  top = "-" * w
 end
 
 def side(w)
-  puts "|" + " " *(w-2)  + "|"
+  side = "|" + " " * (w-2)  + "|\n"
+end
+
+def error
+  "Please enter arguments greater than 2"
 end
 
 def print_rectangle(w,h)
-# if w < 3 || h < 3
-#   error
-# end
-top(w)
-(h-2).times do
-  side(w)
-end
-top(w)
+  return error if w < 3 || h < 3
+  string = top(w) + "\n"
+  (h-2).times {string << side(w)}
+  string << top(w)
 end
 
-# def error
-#   return "Please enter arguments greater than 2"
-# end
+
 
 w = ARGV[0].to_i
 h = ARGV[1].to_i
